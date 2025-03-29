80cc521f2ede0fe10dc1a6b02625c870ba59a989
Elvis Gomez <eogomezf@gmail.com>
2025-03-29 17:07:33 -0600

    Agregar estilos al componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio'
 
 function App() {
   return (
     <div className="App">
-     
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos de FreeTeachCamp</h1>
+        <Testimonio />
+      </div>
+      
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../hojas-de-estilo/Testimonio.css'
 
 function Testimonio() {
   return (
@@ -7,8 +8,10 @@ function Testimonio() {
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">Emma Bostianete en Suecia city</p>
         <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">Me encanta la forma en que los desarrollo por eso estoy desarrollando.</p>
+        <p className="texto-testimonio">Me encanta la forma en que los desarrollo por eso estoy desarrollando. Las clases son muy dinamicas y si he logrado aprender y poner en practica en la empresa. Estudiar los curso de freeTech me ha dado muchas habilidades y la confianza para obtener el trabajo de mis sueños. </p>
       </div>
     </div>
   );
 } 
+
+export default Testimonio;
